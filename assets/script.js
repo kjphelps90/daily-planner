673a0c5a5b55b26ffea586a3815714901648e947
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-08-12 09:49:54 -0400

    added psudocode as I'm going to try to have one function that will set all the times on the left. Also going to try and use the .event function to possibly just have one eventListener for the SAVE buttons.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 // grabbed items
-
+    // clock in header
 var dateTimeEl = $("#date-time");
 console.log(dateTimeEl);
+
+    // grabbing the p tags on the left that will hold the times.
+var time8EL = $("#time-8");
 
 
 
@@ -13,3 +16,32 @@ function displayTime() {
 }
 console.log(moment());
 setInterval(displayTime,1000);
+
+// setting the times that will be assigned to the boxes on the left so that we can compare for the CSS changes.
+
+var hours = [
+    moment("08:00 a", "hh:mm a").format("h a"),
+    moment("09:00 a", "hh:mm a").format("h a"),
+    moment("10:00 a", "hh:mm a").format("h a"),
+    moment("11:00 a", "hh:mm a").format("h a"),
+    moment("12:00 p", "hh:mm a").format("h a"),
+    moment("01:00 p", "hh:mm a").format("h a"),
+    moment("02:00 p", "hh:mm a").format("h a"),
+    moment("03:00 p", "hh:mm a").format("h a"),
+    moment("04:00 p", "hh:mm a").format("h a"),
+    moment("05:00 p", "hh:mm a").format("h a"),
+    moment("06:00 p", "hh:mm a").format("h a"),
+]
+
+console.log(hours[0]);
+console.log(hours[8]);
+
+
+
+
+
+// possible dynamic function that would set the times to each of the boxes on the left.
+
+
+// possible dynamic event listener that would use the .target function in order to have one eventListener for all the buttons.
+
