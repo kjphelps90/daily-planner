25313d87e87569c4f87e62e9f8fba99e338d7eaa
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-08-14 13:10:58 -0400

    updated the getItem from local storage by adding in array of text items that will be used to assign to the different textareas upon loading. Also added in message when saving down that is removed using a setTimeout after 3 seconds.

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,45 @@ $(document).ready(function () {
 var dateTimeEl = $("#currentDay");
 console.log(dateTimeEl.html());
 
-// displaying clock
+    // grabbing all the text-areas in order to update the CSS based on the time of day
+
+var am8 = $("#am8");
+var am9 = $("#am9");
+var am10 = $("#am10");
+var am11 = $("#am11");
+var pm12 = $("#pm12");
+var pm1 = $("#pm1");
+var pm2 = $("#pm2");
+var pm3 = $("#pm3");
+var pm4 = $("#pm4");
+var pm5 = $("#pm5");
+var pm6 = $("#pm6");
+
+var savedMessage = $("#savedMessage");
+
+
+    // setting the times that will be assigned to the boxes on the left so that we can compare for the CSS changes.
+
+    var hours = [
+        {time: moment("8 am", "h a"), lookup: am8},
+        {time: moment("9 am", "h a"), lookup: am9},
+        {time: moment("10 am", "h a"), lookup: am10},
+        {time: moment("11 am", "h a"), lookup: am11},
+        {time: moment("12 pm", "h a"), lookup: pm12},
+        {time: moment("1 pm", "h a"), lookup: pm1},
+        {time: moment("2 pm", "h a"), lookup: pm2},
+        {time: moment("3 pm", "h a"), lookup: pm3},
+        {time: moment("4 pm", "h a"), lookup: pm4},
+        {time: moment("5 pm", "h a"), lookup: pm5},
+        {time: moment("6 pm", "h a"), lookup: pm6}
+    ];
+    
+    console.log(moment().startOf('hour').format("h a"));
+    
+       
+
+
+    // displaying clock
 
 function displayTime() {
     var time = moment().format("MMMM Mo, YYYY[\n]hh:mm:ss a");
@@ -14,39 +52,18 @@ console.log(moment());
 setInterval(displayTime,1000);
 
 
-var am8 = $("#am-8");
-var am9 = $("#am-9");
-var am10 = $("#am-10");
-var am11 = $("#am-11");
-var pm12 = $("#pm-12");
-var pm1 = $("#pm-1");
-var pm2 = $("#pm-2");
-var pm3 = $("#pm-3");
-var pm4 = $("#pm-4");
-var pm5 = $("#pm-5");
-var pm6 = $("#pm-6");
 
 
 
-var hours = [
-    {time: moment("8 am", "h a"), lookup: am8},
-    {time: moment("9 am", "h a"), lookup: am9},
-    {time: moment("10 am", "h a"), lookup: am10},
-    {time: moment("11 am", "h a"), lookup: am11},
-    {time: moment("12 pm", "h a"), lookup: pm12},
-    {time: moment("1 pm", "h a"), lookup: pm1},
-    {time: moment("2 pm", "h a"), lookup: pm2},
-    {time: moment("3 pm", "h a"), lookup: pm3},
-    {time: moment("4 pm", "h a"), lookup: pm4},
-    {time: moment("5 pm", "h a"), lookup: pm5},
-    {time: moment("6 pm", "h a"), lookup: pm6}
-];
+ 
+// if statement comparing the current time to the times associated with the different rows.
 
-console.log(moment().startOf('hour').format("h a"));
-
-for (let i=0; i < hours.length; i++) {
+    for (let i=0; i < hours.length; i++) {
     var currentTime = moment().format("h a");
     console.log(currentTime);
+
+    // going to use addClass and removeClass in the if statement to update the colors.
+
 
     if (hours[i].time.isSame(moment(), "hour")) {
         hours[i].lookup.addClass("present");
@@ -60,42 +77,57 @@ for (let i=0; i < hours.length; i++) {
 }
 
 
+//  eventListener that would use the .target function in order to have one eventListener for all the buttons.
+
+$(":button").on("click", function(event){
+    console.log($(this).siblings());
+    console.log($(this).siblings(".description"));
+    console.log($(this).siblings(".description").val());
+    console.log($(this).siblings(".description").attr("id"));
+
+    var textValue = $(this).siblings(".description").val();
+    var idValue = $(this).siblings(".description").attr("id");
+
+    localStorage.setItem(idValue,textValue);
+
+    // post message to the header that the task has been saved to local storage and then setTimeout to remove the message.
+
+    savedMessage.text("Your message has been saved to local storage!");
+    
+    setTimeout(function(){
+        savedMessage.text("");
+
+    }, 3000);
+
+});
 
 
+function setTasks() {
+    // getItem has to use strings to pull data back in so made a separate array to pull from.
+    var pullBackEL = [
+        "am8", 
+        "am9", 
+        "am10",
+        "am11", 
+        "pm12", 
+        "pm1",
+        "pm2", 
+        "pm3", 
+        "pm4",
+        "pm5",
+        "pm6"
+        ];
 
-console.log(hours[0].lookup);
+    for (let i=0; i < pullBackEL.length; i++) {
+        var savedItem = localStorage.getItem(pullBackEL[i]);
+        console.log(savedItem);
 
-if (hours[0].time.isBefore(moment())) {
-    hours[0].lookup.addClass("past");
+        if (pullBackEL[i]) {
+          hours[i].lookup.text(savedItem);
+        }
+    }
 }
 
-
-// setting the times that will be assigned to the boxes on the left so that we can compare for the CSS changes.
-
-
-
-
-// possible dynamic function that would set the times to each of the boxes on the left.
-
-
-// possible dynamic event listener that would use the .target function in order to have one eventListener for all the buttons.
-
-    // grab the saved event options
-        // save user inputs to a variable and time.
-    // then you want to save to local storage.
-    // add alert to show that it saved to local storage when save button is hit.
-    // create a timeout to remove the notificaiton.
-
-
-    // going to use addClass and removeClass in the if statement to update the colors.
-
-
-
-
-
-
-
-
-
+setTasks();
 
 })
